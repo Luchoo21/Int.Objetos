1f8a449d9faa11a04fa2f13009e463d8359cead8
Matias <matiaslorenzo466@gmail.com>
2021-11-02 15:42:50 -0300

    se actualizo el login

--- TPO-IOO/src/View/Login.java
+++ TPO-IOO/src/View/Login.java
@@ -1,54 +1,75 @@
 package View;
 
 import javax.swing.*;
-import javax.swing.event.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
 
-public class Login extends JFrame implements ActionListener {
-	private JTextField ventana;
-	private JLabel label1, label2;
-	private JButton boton1;
-	public static String texto =" ";
-	
-	public Login() {
-		setLayout(null);
-		setTitle("Bienvenida");
-		getContentPane().setBackground(new Color(100,100,100));
-		
-		label1 = new JLabel("Sistema Reserva de Turnos");
-		label1.setBounds(25,15,300,150);
-		add(label1);
-		
-		label2 = new JLabel("Trabajo Práctico IOO");
-		label2.setBounds(85,375,300,30);
-		label2.setFont(new Font("Andale Mono", 3,18));
-		add(label2);
-		
-		boton1 = new JButton("Ingresar");
-		boton1.setBounds(45,212,200,30);
-		boton1.setBackground(new Color(120,120,120));
-		boton1.setFont(new Font("Andale Mono", 1, 14));
-		boton1.setForeground(new Color(230,140,23));
-		boton1.addActionListener(this);
-		add(boton1);
-		
-	}
+public class Login extends JFrame{
+    private JTextField JTFUsuario;
+    private JPanel panel1;
+    private JPasswordField JPFContra;
+    private JButton registrarseButton;
+    private JButton inicioButton;
+    private JFrame frame;
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	public static void main(String args[]){
-		Login interfaz = new Login();
-		interfaz.setBounds(0,0,350,450);
-		interfaz.setVisible(true);
-		interfaz.setResizable(true);
-		interfaz.setLocationRelativeTo(null);
-	}
 
-	
+    public Login(){
+
+            // Configuracion Ventana
+            frame = new JFrame("Sistema de reserva de turnos");
+            frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+            frame.setPreferredSize(new Dimension(256,256));
+            frame.setResizable(false);
+            frame.getContentPane().setBackground(new Color(100,100,100));
+
+            // Agregamos el otro panel
+
+            frame.add(panel1);
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+
+            // Configuracion Botones
+
+            inicioButton.setBackground(new Color(120,120,120));
+            inicioButton.setFont(new Font("Andale Mono",1,14));
+            inicioButton.setForeground(new Color(230,140,23));
+
+            registrarseButton.setBackground(new Color(120,120,120));
+            registrarseButton.setFont(new Font("Andale Mono",1,14));
+            registrarseButton.setForeground(new Color(230,140,23));
+
+            // Agregamos Acciones
+
+            inicioButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if(String.valueOf(JTFUsuario.getText()).compareTo("admin")==0){
+                        if(String.valueOf(JPFContra.getPassword()).compareTo("1234")==0){}
+                        dispose();
+                        new MenuPrincipal().setVisible(true);
+
+                    } else {
+                        JOptionPane.showMessageDialog(null,"Datos Incorrectos");
+                        getToolkit();
+                        inicioButton.setEnabled(true);
+                    }
+                }
+            });
+
+            registrarseButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                }
+            });
+
+    }
+
+    public static void main(String[] args) {
+        Login interfaz = new Login();
+    }
+
 }
+
